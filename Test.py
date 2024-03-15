e34deea124a6acafa87bc33d2d9680bab131f0f9
mayank21k <142784881+mayank21k@users.noreply.github.com>
2024-03-15 16:43:21 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
 from Main1 import Add
 
 def TestAdd():
-  assert Add(3,4)==7
+  assert Add(3,4)==9
   print("Add Function works correctly")
 
-if __name__=='__main__':
+if__name__=='__main__':
   TestAdd()
